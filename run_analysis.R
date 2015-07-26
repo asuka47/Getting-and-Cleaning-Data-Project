649add54cfaab713413c33cf6ff0d06f1833d3f7
asuka47 <cuizu@outlook.com>
2015-07-27 01:29:14 +0800

    R file upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,55 +1,41 @@
-if (!require("data.table")) {
-  install.packages("data.table")
-}
+library(plyr)
 
-if (!require("reshape2")) {
-  install.packages("reshape2")
-}
 
-require("data.table")
-require("reshape2")
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
 
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
 
-features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+x_data <- rbind(x_train, x_test)
 
-extract_features <- grepl("mean|std", features)
+y_data <- rbind(y_train, y_test)
 
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+subject_data <- rbind(subject_train, subject_test)
 
-names(X_test) = features
 
-X_test = X_test[,extract_features]
+features <- read.table("features.txt")
 
-y_test[,2] = activity_labels[y_test[,1]]
-names(y_test) = c("Activity_ID", "Activity_Label")
-names(subject_test) = "subject"
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
 
-test_data <- cbind(as.data.table(subject_test), y_test, X_test)
+x_data <- x_data[, mean_and_std_features]
 
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+names(x_data) <- features[mean_and_std_features, 2]
 
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-names(X_train) = features
+activities <- read.table("activity_labels.txt")
 
-X_train = X_train[,extract_features]
+y_data[, 1] <- activities[y_data[, 1], 2]
 
-y_train[,2] = activity_labels[y_train[,1]]
-names(y_train) = c("Activity_ID", "Activity_Label")
-names(subject_train) = "subject"
+names(y_data) <- "activity"
 
-train_data <- cbind(as.data.table(subject_train), y_train, X_train)
+names(subject_data) <- "subject"
 
-data = rbind(test_data, train_data)
+all_data <- cbind(x_data, y_data, subject_data)
 
-id_labels   = c("subject", "Activity_ID", "Activity_Label")
-data_labels = setdiff(colnames(data), id_labels)
-melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
 
-tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
-write.table(tidy_data, file = "./tidy_data.txt")
+write.table(averages_data, "averages_data.txt", row.name=FALSE)
